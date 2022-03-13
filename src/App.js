22395540d697637f9ce51d2b5374fd273810d664
sinhadotabhinav <sinhabhinv@gmail.com>
2022-03-13 23:27:37 +0100

    updates to status pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,93 @@
 import './App.css';
-
+const title = 'STATUS PAGE';
+const statusLeft = 'All Systems Operational';
+const statusRight = 'Updated a few minutes ago';
+// eslint-disable-next-line
+const activeIncidents = 0;
+// eslint-disable-next-line
+const activeMaintenance = 0;
+// eslint-disable-next-line
+const daysSinceLast = 50;
+// eslint-disable-next-line
+const eventName = 'Deploy version 4.6.0';
+const eventType = 'Planned Maintenance';
+// eslint-disable-next-line
+const appStatus = 'Operational';
 function App() {
   return (
     <div className='container'>
       <div className='header'>
-        <div className='row'>
-          STATUS PAGES
+        <div className='title'>{ title }</div>
+      </div>
+      <div className='status'>
+        <div className='left'>{ statusLeft }</div>
+        <div className='right'>{ statusRight }</div>
+      </div>
+      <div className='metrics'>
+        <div className='metric'>
+          <div className='value'>{ activeIncidents }</div>
+          <div className='type'>Active Incidents</div>
+        </div>
+        <div className='metric'>
+          <div className='value'>{ activeMaintenance }</div>
+          <div className='type'>Active Maintenances</div>
+        </div>
+        <div className='metric'>
+          <div className='value'>{ daysSinceLast }</div>
+          <div className='type'>Days Since Last Incident</div>
         </div>
       </div>
-      <div
-        className="panel panel-default top_bar make_round"
-        id="statusio_status_bar"
-        style={{ background: '#27AE60', borderBottom: '0' }}
-      >
-        <div className="panel-body" style={{ verticalAlign: 'middle' }}>
-          <div className="row">
-            <div className="col-md-8 col-sm-6 col-xs-12">
-              <strong id="statusbar_text">
-                All Systems Operational
-              </strong>
-            </div>
-            <div className="col-md-4 col-sm-6 hidden-xs">
-              <p className="pull-right white" id="updated_ago">
-                Updated a few seconds ago
-              </p>
-            </div>
+      <div className='integrations'>
+        <div className='application'>
+          <div className='name'>Jira</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+        <div className='application'>
+          <div className='name'>Bitbucket</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+        <div className='application'>
+          <div className='name'>Slack</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+        <div className='application'>
+          <div className='name'>Jenkins</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+        <div className='application'>
+          <div className='name'>Confluence</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+        <div className='application'>
+          <div className='name'>SonarQube</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+        <div className='application'>
+          <div className='name'>Artifactory</div>
+          <div className='app-status'>{ appStatus }</div>
+        </div>
+      </div>
+      <div className='upcoming'>
+        <div className='header'>
+          <div className='event-name'>{ eventName }</div>
+          <div className='event-type'>{ eventType }</div>
+        </div>
+        <div className='event-details'>
+          <div className='details'>
+            <div className='detail-name'>Schedule: </div>
+            <div className='detail-value'>March 15, 2022 02:00 - 02:15 UTC</div>
+          </div>
+          <div className='details'>
+            <div className='detail-name'>Components: </div>
+            <div className='detail-value'>API, Dashboard, Website</div>
+          </div>
+          <div className='details'>
+            <div className='detail-name'>Description: </div>
+            <div className='detail-value'>We will be deploying the next version of Status page. There will be no downtime during this maintenance. We will be deploying the next version of Status page. There will be no downtime during this maintenance. We will be deploying the next version of Status page. There will be no downtime during this maintenance. We will be deploying the next version of Status page. There will be no downtime during this maintenance. We will be deploying the next version of Status page. There will be no downtime during this maintenance. We will be deploying the next version of Status page. There will be no downtime during this maintenance.</div>
           </div>
         </div>
+      </div>
+      <div className='footer'>
       </div>
     </div>
   );
